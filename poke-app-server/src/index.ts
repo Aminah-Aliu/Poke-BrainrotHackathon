ca6f149aa80d9ad59cdaf6144c3b205bf217f1d8
Alexander Cholmsky <84676745+alexcholmsky@users.noreply.github.com>
2024-12-01 11:26:56 -0500

    Update secrets path on be

--- poke-app-server/src/index.ts
+++ poke-app-server/src/index.ts
@@ -7,7 +7,7 @@ import path from 'path';
 // Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(
-    path.resolve(__dirname, '../secrets/poke-87981-firebase-adminsdk-evup9-9a8a45c644.json')
+    path.resolve(__dirname, '/etc/secrets/poke-87981-firebase-adminsdk-evup9-9a8a45c644.json')
   ),
   databaseURL: 'https://poke-87981.firebaseio.com',
 });
